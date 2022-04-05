ba9d0e4ffc7a5cec0ee8d5838c148a6e8df04dee
mahak agrawal <mahak011001@gmail.com>
2022-04-05 18:51:00 +0530

    mongo

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://mahak:root@cluster1.p5j5p.mongodb.net/worshipFirst?retryWrites=true&w=majority").then(()=>{
+    console.log("connected");
+}).catch(err=>{
+    console.log("Failed + " + err);
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port " + 3000);
+})
