a5159a548b0f9974c9b04591009933fda4948892
mahak agrawal <mahak011001@gmail.com>
2022-04-08 14:52:14 +0530

    productModel

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const body = require("body-parser")
 app.use(cors());
 const productCategoryRoute = require("./route/Category");
 const mediafileCategoryRoute= require("./route/mediafileCategoryRoute");
-const mediafileRouter = require("./route/mediafile")
+const mediafileRouter = require("./route/mediafile");
+const productRoute = require("./route/productRoute");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://mahak:root@cluster1.p5j5p.mongodb.net/worshipFirst?retryWrites=true&w=majority").then(()=>{
     console.log("connected");
@@ -17,7 +18,9 @@ app.use(body.urlencoded({ extended: true }));
 app.use(body.json());
 app.use("/product-category",productCategoryRoute);
 app.use("/medialfile-category",mediafileCategoryRoute);
-app.use("/mediafile",mediafileRouter)
+app.use("/mediafile",mediafileRouter);
+app.use("/product",productRoute);
+
 app.listen(3000,()=>{
     console.log("Server is running on port " + 3000);
 });
